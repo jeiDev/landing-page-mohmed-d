7baef6f22dc978db23176c18643c451b041cf267
jeiDev <joseanyeloserrano@gmail.com>
2023-02-13 22:33:20 -0400

    .

--- assets/js/header.js
+++ assets/js/header.js
@@ -13,6 +13,22 @@ function header() {
         const mobileNavShow = document.querySelector('.mobile-nav-show');
         const mobileNavHide = document.querySelector('.mobile-nav-hide');
 
+        const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
+
+        navDropdowns.forEach(el => {
+            el.addEventListener('click', function(event) {
+            if (document.querySelector('.mobile-nav-active')) {
+                event.preventDefault();
+                this.classList.toggle('active');
+                this.nextElementSibling.classList.toggle('dropdown-active');
+
+                let dropDownIndicator = this.querySelector('.dropdown-indicator');
+                dropDownIndicator.classList.toggle('bi-chevron-up');
+                dropDownIndicator.classList.toggle('bi-chevron-down');
+            }
+            })
+        });
+
         document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
             el.addEventListener('click', function (event) {
                 event.preventDefault();
