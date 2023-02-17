29e9453932609cd81dd2b61cdc2b2b01ebd8c6d7
jeiDev <joseanyeloserrano@gmail.com>
2023-02-17 13:54:09 -0400

    completed add map

--- assets/js/home.js
+++ assets/js/home.js
@@ -13,14 +13,64 @@ function drawCiaudFeed() {
                 </a>
             </li>
         `)).join("")
-        console.log({data})
+
         $("#carouselTicker").carouselTicker();
     })
 
 }
 
+function chartMap() {
+    get("public/home/map.json", (res) => {
+        if(!res) return
+        console.log({res})
+
+        am5.ready(function () {
+    
+            var root = am5.Root.new("chartMap");
+    
+            root.setThemes([
+                am5themes_Animated.new(root)
+            ]);
+    
+            var chart = root.container.children.push(am5map.MapChart.new(root, {
+                panX: "rotateX",
+                projection: am5map.geoNaturalEarth1()
+            }));
+    
+            var polygonSeries = chart.series.push(
+                am5map.MapPolygonSeries.new(root, {
+                    geoJSON: am5geodata_worldLow,
+                    exclude: ["AQ"],
+                    fill: "#d1dbdd",
+                    stroke: am5.color(0x000000),
+                    strokeWidth: 2,
+                })
+            );
+    
+            polygonSeries.mapPolygons.template.setAll({
+                tooltipText: "{name}",
+                templateField: "polygonSettings"
+            });
+            
+            if(Array.isArray(res.data) && res.leyends){
+                polygonSeries.data.setAll(res.data.map(item => ({
+                    ...item,
+                    polygonSettings: res.leyends && res.leyends[item.leyend] ? {
+                        fill: am5.color(res.leyends[item.leyend].color)
+                    } : {}
+                })))
+            }
+            
+            chart.appear(1000, 100);
+        });
+    })
+
+}
+
+
 (function ($, undefined) {
     $(window).on("load", function () {
         drawCiaudFeed()
+        chartMap()
     });
 })(jQuery);
