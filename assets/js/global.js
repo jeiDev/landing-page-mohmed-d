e4d9d182a00a2274ba21f34bd437a30c250d95f2
Mohamed <48094431+KMDgit@users.noreply.github.com>
2023-02-18 21:52:06 -0500

    Updated image formats to webp

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,122 +1,151 @@
 const params = new Proxy(new URLSearchParams(window.location.search), {
-    get: (searchParams, prop) => searchParams.get(prop),
-})
+  get: (searchParams, prop) => searchParams.get(prop),
+});
 
 function get(url, callback) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            try {
-                callback(JSON.parse(this.responseText))
-            } catch (error) {
-                console.log({error})
-                callback(this.responseText)
-            }
-        } else {
-            callback(null)
-        }
-    };
-    xhttp.open("GET", url, true)
-    xhttp.send()
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      try {
+        callback(JSON.parse(this.responseText));
+      } catch (error) {
+        console.log({ error });
+        callback(this.responseText);
+      }
+    } else {
+      callback(null);
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
 }
 
 function drawMenuInfo(parent, data) {
-    parent.innerHTML = `
-        ${data.image ? (`
-            <img src="${data.image || 'assets/img/image-not-found.png'}" alt="" class="img-fluid services-img">
-        `) : ""}
+  parent.innerHTML = `
+        ${
+          data.image
+            ? `
+            <img src="${
+              data.image || "assets/img/image-not-found.webp"
+            }" alt="" class="img-fluid services-img">
+        `
+            : ""
+        }
         <div>
             <h3>${data.title}</h3>
             <div class="box-list">
-                ${data.adresse ? (`
+                ${
+                  data.adresse
+                    ? `
                     <div>${data.adresse}</div>
-                `) : ""}
+                `
+                    : ""
+                }
 
-                ${data.aggreement_year ? (`
+                ${
+                  data.aggreement_year
+                    ? `
                     <div>${data.aggreement_year}</div>
-                `) : ""}
+                `
+                    : ""
+                }
             </div>
         </div>
         <p>${data.description}</p>
         <ul>
-            ${Array.isArray(data.items) && data.items.length ? (
-                data.items.map(item => (`
+            ${
+              Array.isArray(data.items) && data.items.length
+                ? data.items
+                    .map(
+                      (item) => `
                     <li><i class="bi bi-check-circle"></i> <span>${item}</span></li>
-                `)).join("")
-            ) : ""}
+                `
+                    )
+                    .join("")
+                : ""
+            }
             
-            ${data.email ? (`
+            ${
+              data.email
+                ? `
                 <li><strong class="list-strong">Email: </strong><span>${data.email}</span></li>
-            `) : ""}
-            ${data.phone ? (`
+            `
+                : ""
+            }
+            ${
+              data.phone
+                ? `
                 <li><strong class="list-strong">Phone: </strong><span>${data.phone}</span></li>
-            `) : ""}
+            `
+                : ""
+            }
             
         </ul>
         
-    `
+    `;
 }
 
-function removeActive(listBox){
-    listBox.querySelectorAll("a").forEach(a => {
-        a.classList.remove("active")
-    })
+function removeActive(listBox) {
+  listBox.querySelectorAll("a").forEach((a) => {
+    a.classList.remove("active");
+  });
 }
 
 function drawListMenuInfo(contentBox, listBox, data, active = false) {
-    if(active){
-        removeActive(listBox)
-    }
-    
-    let a = document.createElement("a")
+  if (active) {
+    removeActive(listBox);
+  }
 
-    a.innerText = data.title || ""
-    a.style.cursor = "pointer"
+  let a = document.createElement("a");
 
-    if (active) {
-        a.classList.add("active")
-        drawMenuInfo(contentBox, data)
-    }
+  a.innerText = data.title || "";
+  a.style.cursor = "pointer";
 
-    a.onclick = e => {
-        e.preventDefault()
-        e.stopPropagation()
+  if (active) {
+    a.classList.add("active");
+    drawMenuInfo(contentBox, data);
+  }
 
-        removeActive(listBox)
+  a.onclick = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
 
-        a.classList.add("active")
-        drawMenuInfo(contentBox, data)
+    removeActive(listBox);
 
-    }
+    a.classList.add("active");
+    drawMenuInfo(contentBox, data);
+  };
 
-    listBox.appendChild(a)
+  listBox.appendChild(a);
 }
 
 function googleTranslateElementInit() {
-    let time;
+  let time;
 
-
-   time = setInterval(() => {
+  time = setInterval(() => {
     let el = document.getElementById("google_translate_element");
-    if(!el) return 
-    console.log("yex")
-    clearInterval(time)
+    if (!el) return;
+    console.log("yex");
+    clearInterval(time);
 
     new google.translate.TranslateElement(
-        {pageLanguage: 'fr', includedLanguages : 'fr,en,ar'},
-        'google_translate_element'
+      { pageLanguage: "fr", includedLanguages: "fr,en,ar" },
+      "google_translate_element"
     );
-   }, 100)
+  }, 100);
 }
 
-function getRSS(url, callback){
-    var data = { rss: url }
-    $.get(`https://api.rss2json.com/v1/api.json?rss_url=${data.rss}`, function( data ) {
-        callback(data)
-    });
+function getRSS(url, callback) {
+  var data = { rss: url };
+  $.get(
+    `https://api.rss2json.com/v1/api.json?rss_url=${data.rss}`,
+    function (data) {
+      callback(data);
+    }
+  );
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    includeHTML();
-    header();
-})
+  includeHTML();
+  header();
+});
